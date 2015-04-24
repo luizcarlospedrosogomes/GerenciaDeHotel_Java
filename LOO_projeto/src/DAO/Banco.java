643afa40a432af30051af5f9bab0702314295432
luizcarlospedrosogomes <lc.pg@hotmail.com>
2015-04-24 19:44:53 -0300

    Update Banco.java

--- LOO_projeto/src/DAO/Banco.java
+++ LOO_projeto/src/DAO/Banco.java
@@ -18,20 +18,20 @@ import java.sql.Statement;
 public class Banco {
 
     Connection con;
-    String url = "jdbc:postgresql://127.0.0.1:5432/LOO_projeto";
-    String usuario = "postgres";
-    String senha = "sa";
-    ResultSet dados;
-    Statement sessao;
+   // String url = "jdbc:postgresql://127.0.0.1:5432/LOO_projeto";
+//    String usuario = "postgres";
+  //  String senha = "sa";
+//ResultSet dados;
+ //   Statement sessao;
 
     public Banco() throws ClassNotFoundException, SQLException {
-        conectar();
+       // conectar();
     }
 
     public void conectar() throws ClassNotFoundException, SQLException {
 
-        Class.forName("org.postgresql.Driver");
-        con = DriverManager.getConnection(url, usuario, senha);
+      //  Class.forName("org.postgresql.Driver");
+      //  con = DriverManager.getConnection(url, usuario, senha);
         
 
     }
